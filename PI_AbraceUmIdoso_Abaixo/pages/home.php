cc7f2d3c2fb0d10e3ddbffebdb52af87700c4b5d
NayNath <nathy.zulmira@gmail.com>
2026-04-14 13:36:31 -0300

    add video

--- PI_AbraceUmIdoso_Abaixo/pages/home.php
+++ PI_AbraceUmIdoso_Abaixo/pages/home.php
@@ -22,7 +22,21 @@ require_once __DIR__ . '/../includes/layout_top.php';
       <div class="missao-item"><div class="icone-circulo"><img src="../assets/img/idosos.png" alt="Idosos"></div><strong>Carinho</strong></div>
       <div class="missao-item"><div class="icone-circulo"><img src="../assets/img/pessoas.png" alt="Pessoas"></div><strong>Comunidade</strong></div>
     </div>
+
+    <div class="video">
+      <iframe 
+        src="https://www.youtube.com/embed/LM3KHgIkyMU?si=OqSltFhbrEIVJOXE" 
+        title="YouTube video player" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+      </iframe>
+    </div>
   </section>
+  
+    
+  
+
 
   <section class="cards-grid fade-in">
     <article class="card center"><h2>Recuperação de Senha</h2><p>Login com sessão e recuperação de senha por token e e-mail.</p><a class="card-button" href="login.php">Acessar</a></article>
